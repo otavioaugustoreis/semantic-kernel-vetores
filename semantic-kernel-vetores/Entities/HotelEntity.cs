5b25ffdaa616bd1d8a747555f2283e519d0b5a6a
Otavio Augusto Reis Almeida <otavio.almeida@pwi.com.br>
2025-04-22 15:17:55 -0300

    Add project files.

---
+++ semantic-kernel-vetores/Entities/HotelEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.VectorData;
+
+namespace semantic_kernel_vetores.Entities
+{
+    public class HotelEntity
+    {
+        [VectorStoreRecordKey]
+        public ulong HotelId { get; set; }
+
+        [VectorStoreRecordData(IsFilterable = true)]
+        public string HotelName { get; set; }
+
+        [VectorStoreRecordData(IsFullTextSearchable = true)]
+        public string Description { get; set; }
+
+        [VectorStoreRecordVector(Dimensions: 4, DistanceFunction.CosineSimilarity, IndexKind.Hnsw)]
+        public ReadOnlyMemory<float>? DescriptionEmbedding { get; set; }
+
+        [VectorStoreRecordData(IsFilterable = true)]
+        public string[] Tags { get; set; }
+    }
+}
