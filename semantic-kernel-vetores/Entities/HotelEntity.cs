8852b48592e6ba4788866f231b8e1088c86a1b0b
Otavio Augusto Reis Almeida <otavio.almeida@pwi.com.br>
2025-04-23 16:49:45 -0300

    ultimo commit pc N000B

--- semantic-kernel-vetores/Entities/HotelEntity.cs
+++ semantic-kernel-vetores/Entities/HotelEntity.cs
@@ -2,20 +2,51 @@
 
 namespace semantic_kernel_vetores.Entities
 {
+    #region COMO_FUNCIONA
+    /*
+        Cenário de Busca Semântica
+        Usuário busca: "hotel aconchegante para lua de mel".
+
+        Sistema:
+
+        Converte a consulta em um vetor (embedding).
+
+        Compara com DescriptionEmbedding dos hoteis usando similaridade de cosseno.
+
+        Retorna os mais relevantes, mesmo sem correspondência exata de palavras.
+
+        Vantagens
+        Encontra relações conceituais (ex.: "aconchegante" → "romântico", "intimista").
+
+        Combina buscas vetoriais com filtros tradicionais (ex.: tags "luxo" + semântica "vista para o mar").
+     */
+    #endregion
     public class HotelEntity
     {
+        //Chave primária
         [VectorStoreRecordKey]
         public ulong HotelId { get; set; }
 
+        //Permite filtrar por esse campo (ex.: WHERE HotelName = "Copacabana Palace").
         [VectorStoreRecordData(IsFilterable = true)]
         public string HotelName { get; set; }
 
+        //Habilita busca por texto completo (ex.: encontrar "hotel à beira-mar" mesmo se a descrição usar "frente para o oceano").
         [VectorStoreRecordData(IsFullTextSearchable = true)]
         public string Description { get; set; }
 
+        //O Coração Vetorial
+        //Armazena o embedding (vetor numérico) da descrição do hotel.
+        /*
+         Parâmetros:
+          Dimensions: 4: Tamanho do vetor (4 é apenas ilustrativo - na prática, use 768, 1024, etc.).
+          DistanceFunction.CosineSimilarity: Métrica para comparar vetores (outras opções: Euclidean, DotProduct).
+          IndexKind.Hnsw: Algoritmo de indexação para buscas eficientes (Hierarchical Navigable Small World).
+        */
         [VectorStoreRecordVector(Dimensions: 4, DistanceFunction.CosineSimilarity, IndexKind.Hnsw)]
         public ReadOnlyMemory<float>? DescriptionEmbedding { get; set; }
 
+        //Útil para filtros múltiplos (ex.: Tags.Contains("luxo")).
         [VectorStoreRecordData(IsFilterable = true)]
         public string[] Tags { get; set; }
     }
--- semantic-kernel-vetores/Program.cs
+++ semantic-kernel-vetores/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Qdrant.Client;
+
 Console.WriteLine("Hello, World!");
+
+var vectorStore = new QdrantClient("localhost");
